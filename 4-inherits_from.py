55151850fb91f305b59fb92558c54dce6813a3e0
aoairende <aoairende@gmail.com>
2023-09-13 03:30:50 +0100

    I/O

--- 4-inherits_from.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-"""Module defines a function"""
-
-
-def inherits_from(obj, a_class):
-    """checks class instance"""
-
-    return issubclass(type(obj), a_class) and type(obj) != a_class
--- 5-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-"""Module defines a function"""
-
-
-class BaseGeometry:
-    """Defines a geometry class"""
-
-    pass
